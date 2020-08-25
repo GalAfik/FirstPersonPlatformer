b530fe48f58588e6291d2593a3e3b3ab9b91db48
Gal Afik <gal.p.afik@gmail.com>
2020-08-25 00:30:11 -0400

    First commit - added a basic first person controller.

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+	public float mouseSensitivity = 100f;
+	public Transform playerBody;
+
+	private float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		// Lock and hide the mouse
+		Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+		float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+		xRotation -= mouseY;
+		// Clamp the vertical camera movement so the player can't look behind them
+		xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+		// Rotate the camera on the x axis
+		transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+		// Rotate the player object on the y axis
+		playerBody.Rotate(Vector3.up * mouseX);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	private CharacterController controller;
+
+	public float speed = 12f;
+	public float jumpHeight = 3f;
+	public float gravityModifier = 1f;
+
+	public Transform groundCheck;
+	public float groundDistance = 0.1f;
+	public LayerMask groundMask;
+
+	private Vector3 velocity;
+	private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		controller = GetComponent<CharacterController>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		// Raycast - test if the player is on the ground
+		isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+		// Get strafing input
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+
+		// Use the player's local left/right and forward/back axes to move, rather than the absolute axes
+		Vector3 move = transform.right * x + transform.forward * z;
+
+		// Apply movement vector
+		controller.Move(move * speed * Time.deltaTime);
+
+		// Handle gravity
+		velocity.y += Physics.gravity.y * Time.deltaTime;
+
+		// Reset vertical velocity when grounded
+		if (isGrounded && velocity.y < 0) velocity.y = -2f;
+
+		// Handle Jumping
+		if (Input.GetButtonDown("Jump") && isGrounded)
+		{
+			velocity.y = Mathf.Sqrt(jumpHeight * -2f * (Physics.gravity.y / gravityModifier));
+		}
+
+		// Apply vertical velocity
+		controller.Move(velocity * gravityModifier * Time.deltaTime);
+	}
+}
